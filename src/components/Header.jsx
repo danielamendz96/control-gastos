c30a53488dbc14beecb64cffc267ecabad5f0124
daniela <danielamendoz96@gmail.com>
2023-02-07 00:25:43 -0400

    primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Header>
+        <h1>control de gastos</h1>
+    </Header>
+  );
+}
+
+export default Header;
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoPresupuesto = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NuevoPresupuesto;
